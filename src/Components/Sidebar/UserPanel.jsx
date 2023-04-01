cb9562501cb1dd98819112a4cf2009063a8d24bb
name <sakawsar70@gmail.com>
2023-04-01 11:58:11 +0600

    fixed sidepanel

--- src/Components/Sidebar/UserPanel.jsx
+++ src/Components/Sidebar/UserPanel.jsx
@@ -62,9 +62,9 @@ const UserPanel = () => {
                 data.map((item,index) => (
                     <li className='list-none ' key={index} >
                         <NavLink to={item.link} 
-                        className={({ isActive }) => isActive ? 'lg:px-6 lg:py-4 px-2 py-4  w-full block rounded-r-full   lg:hover:px-7 text-white bg-blue-600': 'lg:px-6 lg:py-4 px-2 py-4  w-full block rounded-r-full  lg:hover:px-7 text-black hover:bg-gray-200'}
+                        className={({ isActive }) => isActive ? 'lg:px-6 lg:py-4 px-2 py-4  w-full block rounded-r-full   lg:hover:px-7 text-white bg-blue-600': 'lg:px-6 lg:py-4 px-2 py-4  w-full block rounded-r-full  lg:hover:px-7 text-black hover:bg-gray-200 md:text-base text-sm'}
                         >
-                            <FontAwesomeIcon icon={item.icon} className=' w-4 h-4 mr-2'/>
+                            <FontAwesomeIcon icon={item.icon} className=' md:w-4 md:h-4 w-3 h-3 mr-2'/>
                             {sidebar.sideBar &&sidebar.screenSize.dynamicWidth>640?  null: item.title}
                         </NavLink>
                     </li>
