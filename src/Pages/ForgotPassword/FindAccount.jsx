b575e7362a2f6ecf70380c2a40b0d1d34f373927
name <sakawsar70@gmail.com>
2023-04-03 11:14:00 +0600

    find account jsx

---
+++ src/Pages/ForgotPassword/FindAccount.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FindAccount = () => {
+    return (
+        <div className=' h-[100vh] flex justify-center items-center'>
+            
+        </div>
+    );
+};
+
+export default FindAccount;
---
+++ src/Pages/ForgotPassword/ForgotPasssword.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FindAccount from './FindAccount';
+
+const ForgotPasssword = () => {
+    return (
+        <div>
+            <FindAccount/>
+        </div>
+    );
+};
+
+export default ForgotPasssword;
