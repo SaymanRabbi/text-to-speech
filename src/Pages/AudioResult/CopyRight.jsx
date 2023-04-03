40e83e427e6ebeee714b06b5764622f61c4e4430
name <sakawsar70@gmail.com>
2023-04-03 14:07:46 +0600

    dynamic year

--- src/Pages/AudioResult/CopyRight.jsx
+++ src/Pages/AudioResult/CopyRight.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const CopyRight = () => {
+    const year = new Date().getFullYear()
     return (
         <div className=' p-16 bg-[#FFFFFF] mt-20'>
             <div className='flex justify-center items-center'>
-              <h2>Copyright © 2023 <a href='https://techsroute.com/' target='_blank' className=' font-bold text-blue-500'>Techs Route</a> . All rights reserved</h2>
+              <h2>Copyright © {year} <a href='https://techsroute.com/' target='_blank' className=' font-bold text-blue-500'>Techs Route</a> . All rights reserved</h2>
             </div>
         </div>
     );
